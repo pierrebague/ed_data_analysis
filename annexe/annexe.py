d536e751f0b9993cf2788314e0b8adcd56427277
pierrebague <39726574+pierrebague@users.noreply.github.com>
2021-04-01 15:53:19 +0200

    Add files via upload

--- annexe/annexe.py
+++ annexe/annexe.py
@@ -4,9 +4,12 @@ import pandas as pd
 fichiers = ["EdStatsCountry.csv","EdStatsCountry-Series.csv","EdStatsData.csv","EdStatsFootNote.csv","EdStatsSeries.csv"]
 localisation ='F:/cour/OC/projet2/'
 
+
+
 def ouvre_csv(num_fichier):
     fichierLu = pd.read_csv(localisation + fichiers[num_fichier])
     fichierLu = fichierLu.dropna(axis = 'columns', how = 'all')
+    print(fichiers[num_fichier], " is open")
     return(fichierLu)
 
 def garde_nombre(df):
@@ -42,11 +45,29 @@ def premiere_epuration(df,nbLignes,ratio):
             dfEpure[listIndex[index]] = df[listIndex[index]]
     return(dfEpure)
 
+
+def print_some_info(df)
+	print("shape of the dataframe",df.shape)
+	for column in df.columns:
+		print('\n' + column)
+		print(df[column].nunique() , "n uniques")
+		print(df[column].isna().sum(), "somme des non nul dans la colonne")
+		print("Some exemples :\n"dataFrame.sample(10,random_state = 16))
+	
+	
 def test_nunique_isna(df):
-    for column in df.columns:
-        print('\n' + column)
-        print(df[column].nunique() , "n uniques")
-        print(df[column].isna().sum(), "somme des non nul dans la colonne")
+	print("shape of the dataframe",df.shape)
+	for column in df.columns:
+		print('\n' + column)
+		print(df[column].nunique() , "n uniques")
+		print(df[column].isna().sum(), "somme des non nul dans la colonne")
+	return()
+
+
+
+def print_samples(dataFrame):
+    print(dataFrame.sample(5))
+    return()
         
 def sup_empty_row(dfdate,dfNormal):
     dfNormalCopy = dfNormal.copy()
@@ -79,3 +100,6 @@ def plotSmt(df,nbValueByGraphe):
             min = data.min()
             x= np.linspace(min,max,nbValues)
             ax.plot(x,data)
+    return()
+
+            
